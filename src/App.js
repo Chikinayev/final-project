73ece68824f830bf5457bf121f8a1081cec1fe1c
Chikinayev <62277693+Chikinayev@users.noreply.github.com>
2021-12-24 22:30:56 +0600

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+// import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
+// import PostItem from './components/PostItem';
+import PostList from './components/PostList';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript 1', body: 'Description'},
+    {id: 2, title: 'JavaScript 2 ', body: 'Description'},
+    {id: 3, title: 'JavaScript 3', body: 'Description'},
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+          <input type="text" placeholder="Название поста"/>
+      </form>
+      <PostList posts={posts} />
     </div>
   );
 }
