7c240fa918aa9313c496b935b639d3a4dac655b7
Chikinayev <62277693+Chikinayev@users.noreply.github.com>
2021-12-27 03:20:04 +0600

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,89 +1,33 @@
+import {BrowserRouter } from "react-router-dom";
+import AppRouter from "./components/AppRouter";
+import Navbar from "./components/UI/Navbar/Navbar";
+import { AuthContext } from "./context";
 import { useEffect, useState } from "react";
-import './styles/App.css';
-import PostList from './components/PostList';
-import PostForm from "./components/PostForm";
-import PostFilter from "./components/PostFilter";
-import MyModal from "./components/UI/MyModal/MyModal";
-import MyButton from "./components/UI/button/MyButton";
-import { usePosts } from "./hooks/usePosts";
-import PostService from "./API/PostService";
-import Loader from "./components/UI/Loader/Loader";
-import { useFetching } from "./hooks/useFetching";
-import {getPageCount} from "./utils/pages";
-import Pagination from "./components/UI/pagination/Pagination";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [filter, setFilter] = useState({sort: '', query: ''});
-  const [modal, setModal] = useState(false);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [totalPages, setTotalPages] = useState(0);
-  const [limit, setLimit] = useState(10);
-  const [page, setPage] = useState(1);
+  const [isAuth, setIsAuth]=useState(false);
+  const [isLoading, setIsLoading] =useState(true);
 
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async(limit, page)=>{
-    const response = await PostService.getAll(limit, page)
-    setPosts(response.data)
-    const totalCount = response.headers['x-total-count']
-    setTotalPages(getPageCount(totalCount, limit));
-  })
-
-  
   useEffect(()=>{
-    fetchPosts(limit, page)
+    if(localStorage.getItem('auth')){
+      setIsAuth(true)
+      
+    }
+    setIsLoading(false)
   },[])
 
-  const createPost = (newPost)=>{
-    setPosts([...posts, newPost]);
-    setModal(false);
-  }  
-  
-
-  const removePost = (post)=>{
-    setPosts(posts.filter(p=>p.id !== post.id))
-  }
-
-  const changePage =(page)=>{
-    setPage(page)
-    fetchPosts(limit, page)
-  }
-  
-
   return (
-    <div className="App">
-      <MyButton style={{marginTop: 30}}onClick={()=>{setModal(true)}}>
-        Создать пользователя
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
-      
-      <hr style={{margin: '15px 0'}}></hr>
-      
-      <PostFilter 
-        filter={filter} 
-        setFilter={setFilter} 
-      />
-      {
-        postError &&
-          <h1>Произошла Ошибка ${postError}</h1>
-      }
-      {
-        isPostsLoading
-        ? 
-          <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}>
-            <Loader />
-          </div>
-        :
-          <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
-      }
-      <Pagination 
-        page={page}
-        changePage={changePage}
-        totalPages={totalPages}
-      />
-      
-    </div>
+    <AuthContext.Provider value={{
+      isAuth, 
+      setIsAuth,
+      isLoading
+    }}>
+      <BrowserRouter>
+        <Navbar />
+        <AppRouter />
+      </BrowserRouter>
+    </AuthContext.Provider>
+    
   );
 }
 
---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const About = ()=> {
+    return(
+        <h1>
+            это страница про меня
+        </h1>
+    );
+};
+
+export default About;
